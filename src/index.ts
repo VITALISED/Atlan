752f3fb9b43cac2be4677ca147e8ec1cf73ec97c
Will <39478251+VITALISED@users.noreply.github.com>
2023-07-20 23:00:50 +1000

    Add express types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi!");
+import { default as express } from "express"
+
+const app = express()
+const port = 8888
+
+app.get('/', (req, res) => {
+    res.send(200)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
---
+++ src/routes/blobserve.ts
@@ -0,0 +1,6 @@
+import { Application } from "express"
+
+export default void function(app: Application)
+{
+
+}
